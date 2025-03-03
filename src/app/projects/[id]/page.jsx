4eb5ef95e89e93e5efe06e8678fdc1022f5a2682
goutham4126 <goutham4126@gmail.com>
2025-03-03 19:37:26 +0530

    fix error

--- src/app/projects/[id]/page.jsx
+++ src/app/projects/[id]/page.jsx
@@ -1,11 +1,11 @@
 import Playground from "../_components/Playground";
 
 async function Page({ params }) {
-  const projectId = params.id;
+  const { id } = await params;
 
   return (
     <div>
-      <Playground template="static" projectId={projectId} />
+      <Playground template="static" projectId={id} />
     </div>
   );
 }
